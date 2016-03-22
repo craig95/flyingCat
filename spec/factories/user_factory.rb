182b5b10f0740d3c9245323851f1f3502cbdc16a
craig95 <u15029779@tuks.co.za>
2016-03-23 00:16:43 +0200

    Automated tests.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,14 +2,14 @@ FactoryGirl.define do
   sequence(:email) { |n| "user#{n}@example.com" }
   factory :user do
     email
-    password "1234567890"
+    password "qwerty12345"
     first_name "Peter"
     last_name "Example"
     admin false
   end
   factory :admin, class: User do
-    email
-    password "1234567890"
+    email 
+    password "qwerty12345"
     first_name "Peter"
     last_name "Example"
     admin true
