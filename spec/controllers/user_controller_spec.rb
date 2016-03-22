25bfed61bbff0bbe49b6bc58748cf2df384705c8
craig95 <u15029779@tuks.co.za>
2016-03-22 15:11:04 +0200

    Added Unit tests and functional tests.

---
+++ spec/controllers/user_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+
+  before do
+    @user1 = User.create!(email: "testing1@mail.com", password: "testpassword")
+    @user2 = User.create!(email: "testing2@mail.com", password: "testpassword")
+  end
+
+  describe "GET #show" do
+
+    context "User is logged in" do
+
+      before do
+        sign_in @user1
+      end
+
+      it "loads correct user details" do
+        get :show, id: @user1.id
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq @user1
+      end
+
+      it "first user cannot access second users show page" do
+        get :show, id: @user2.id
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+
+    end
+
+    context "No user is logged in" do
+      it "redirects to login" do
+          get :show, id: @user1.id
+          expect(response).to redirect_to("http://test.host/login")
+      end
+    end
+
+  end
+end
