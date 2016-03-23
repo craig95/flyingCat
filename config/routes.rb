4f0b93eeb64149ce8f6517e1e639404852d38488
craig95 <u15029779@tuks.co.za>
2016-03-23 16:52:41 +0200

    Added the payments controller to process payments and create orders

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
 
   post 'static_pages/thank_you'
 
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+  post 'payments/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
