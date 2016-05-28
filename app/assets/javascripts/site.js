ea034e8c1435d8a97c78b44280aa667306e4c5d4
craig95 <u15029779@tuks.co.za>
2016-05-28 23:16:07 +0200

    Comments are now added using AJAX. When the comment is posted AJAX is used to render the comment partial which contains the new review.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,14 @@
+function initializeRaty() {
+  $('#product-image').elevateZoom();
+  $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+}
+
 $(document).on('ready page:load', function(){
-    $('#product-image').elevateZoom();
-    $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+  initializeRaty();
 });
